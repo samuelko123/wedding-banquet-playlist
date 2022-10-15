36170b29ced7b086f6b0859e7f8de81b25bc570e
Samuel Ko <koshunyin@gmail.com>
2022-10-15 13:39:58 +1100

    extract data to separate file

---
+++ src/data/album.js
@@ -0,0 +1,54 @@
+export const album = {
+	sections: [
+		{
+			name: 'Section A',
+			songs: [
+				{
+					name: 'Song A1',
+					src: '/A1.mp3',
+				},
+				{
+					name: 'Song A2',
+					src: '/A2.mp3',
+				},
+				{
+					name: 'Song A3',
+					src: '/A3.mp3',
+				},
+				{
+					name: 'Song A4',
+					src: '/A4.mp3',
+				},
+				{
+					name: 'Song A5',
+					src: '/A5.mp3',
+				},
+			],
+		},
+		{
+			name: 'Section B',
+			songs: [
+				{
+					name: 'Song B1',
+					src: '/B1.mp3',
+				},
+				{
+					name: 'Song B2',
+					src: '/B2.mp3',
+				},
+				{
+					name: 'Song B3',
+					src: '/B3.mp3',
+				},
+				{
+					name: 'Song B4',
+					src: '/B4.mp3',
+				},
+				{
+					name: 'Song B5',
+					src: '/B5.mp3',
+				},
+			],
+		},
+	],
+}
